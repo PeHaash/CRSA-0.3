e2354be8d56d3c6137038cdad513c46c71a5ab35
Pedram Hashemi <PedramHashemi75@gmail.com>
2024-02-28 23:57:58 +0000

    fbt is now deprecated

--- include/detenvi_fbt.h
+++ include/detenvi_fbt.h
@@ -7,3 +7,27 @@
 
  */
 
+
+
+/*
+  rules of spaces (decleared in the gird):
+    B: blocked - not accessible to us, outside walls, other rooms, etc...
+    O: Outside - not accessible to us, but we can put windows to them
+    A: Access - not accessible to build, but we can put Doors to them
+    F: Free - Free for us to work on
+*/
+
+// -1: unscored
+// 0: worst possible
+// 1: great!
+struct Objectives{
+  double NoRoomInTheBOA; // blocked, 
+  double HasEntrance;
+  double HasOnlyOneEntrance;
+  double AllSpacesAreConnected;
+};
+// make a pointer of objective, points at the array came from python
+
+
+
+// #include <cstdint>
