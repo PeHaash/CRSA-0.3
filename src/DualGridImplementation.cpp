264a4f14a7fa7566cf1d631fec87a836e93eecc0
Pedram Hashemi <PedramHashemi75@gmail.com>
2024-03-02 01:34:37 +0000

    debug

--- src/DualGridImplementation.cpp
+++ src/DualGridImplementation.cpp
@@ -114,6 +114,7 @@ DualGridImplementer::ExportPrototype DualGridImplementer::ImplementationCore(boo
 
 	for (uint32_t i = 0; i < Width; i++)
 		for(uint32_t j = 0; j < Height; j++){
+			auto s = WhiteSpace.data[ijCoordsto1D(i, j)];
 			// char ssIndex = ConvertToBase(n, WhiteSpace.data[ijCoordsto1D(i, j)]) - 1; // -1: empty shit, 0...s*2: white rooms
 			// if(ssIndex != -1) WhiteSpaceList[ssIndex].UpdateWith(i, j);
 		}
